8c8e09432b601bda24a05d876af87b6ebf000ac9
Brian Liu <b_liu72@yahoo.com>
2016-03-31 03:01:14 -0400

    Added resume and project pages. Made content appear below header.

--- header.js
+++ header.js
@@ -8,6 +8,7 @@ $(document).ready( function(){
     infoh = $('#info').height();
     h = $('#shrinker').height();
     sh = $("#spacer").height();
+    $('#main').css('padding-top', (headerh + 40) + 'px');
 })
 
 $(window).scroll(function()
@@ -33,17 +34,16 @@ $(window).scroll(function()
     if((h-sc) < 20) {
 
         if (state !== 3) {
-            //$('.header').css({'padding-top':'0px',});
             $('#shrinker').animate({"height":'0px'},250);
             $('#info').animate({"height":'0px'},250);
             $('.header').stop().animate({'padding-top':'0px'},250);
             $('.name').stop().animate({'font-size': '18px', 'padding-top':'0px'},250);
             $('.navi').css({'position': 'relative', 'bottom': '0', 'left': '0'})
+            $('#main').animate({'padding-top': (sc + 64) + 'px'}, 250);
         }
 
         $('#info').animate({'opacity':0},0);
         state = 3;
-
 
     }
 
@@ -56,7 +56,7 @@ $(window).scroll(function()
             $('.header').stop().animate({'padding-top':'40px'},250);
             $('.name').stop().animate({'font-size': '400%', 'padding-top':'0px'},250, 'swing', function() {$('.navi').css({'position': 'absolute', 'bottom': '22', 'left': '15'})});
             $('#info').animate({"height":infoh + 'px'},250);
-            
+            $('#main').animate({'padding-top': (40 + headerh) + 'px'}, 250);
         }
         state = 1;
     }
@@ -64,5 +64,7 @@ $(window).scroll(function()
     var op = 1-((headerh-1.5*sc)/headerh*.8)
 
     $('.header').css({'background': 'linear-gradient(rgba(76,185,80,' + op + '), rgba(76, 185, 80, ' + op + ')), url("image2.jpg")', 'background-size': '100%'});
+
+
 
 });
